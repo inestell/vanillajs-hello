cfd9b65b63d4349601504aca32f4b7f9ceebda8c
inestell <166044521+inestell@users.noreply.github.com>
2024-04-19 14:33:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+    console.log("Hello Rigo from the console! ");
+  });
 };
+
+function generateExcuse() {
+  let who = ["My car", "A bee", "My cousin", "My nose"];
+  let verb = ["ate", "drove", "shook", "pushed"];
+  let thing = ["a nut", "my pencil", "an ice-cream", "a butterfly"];
+  let when = [
+    "yesterday",
+    "when I was born",
+    "last Summer",
+    "on Christmas Eve"
+  ];
+
+  let whoInd = Math.floor(Math.random() * who.length);
+  let verbInd = Math.floor(Math.random() * verb.length);
+  let thinInd = Math.floor(Math.random() * thing.length);
+  let whenInd = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[whoInd] +
+    " " +
+    verb[verbInd] +
+    " " +
+    thing[thinInd] +
+    " " +
+    when[whenInd];
+
+  return excuse;
+}
